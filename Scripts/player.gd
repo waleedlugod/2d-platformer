22287cfc6abc19d495c387df1cc97bf2c8027f47
beatriz.bernardino <beatriz.bernardino@student.ateneo.edu>
2024-09-25 00:22:45 +0800

    camera and map implemented
    camera and viewport size should still be fixed so that it is more fit
    
    map is a rough draft. Please let me know if I should make it larger.

---
+++ Scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var gravity = 30
+@export var jump_force = 300
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+		
+	if Input.is_action_just_pressed("up"):
+		velocity.y = -jump_force
+	
+	var horizontal_direction = Input.get_axis("right", "left")
+	
+	velocity.x = -(speed) * horizontal_direction
+	
+	move_and_slide()
+
+
+	print(velocity)
